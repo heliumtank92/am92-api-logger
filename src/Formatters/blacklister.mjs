e781f42ebc2356cf81d120eb4e32b34938a63d7b
Mehul Agarwal <mehulagarwal@Mehuls-MacBook-Pro.local>
2022-12-28 14:48:55 +0530

    Add Debug Mode for Prod Env

--- src/Formatters/blacklister.mjs
+++ src/Formatters/blacklister.mjs
@@ -1,49 +1,54 @@
 import crypto from 'crypto'
 
 const isProduction = process.env.NODE_ENV === 'production'
+const disableBlacklist = process.env.DEBUG === '*' || process.env.DEBUG.includes('apiLogger:*')
 
 export default function blacklister (logObject = {}) {
-  if (!isProduction) { return logObject }
+  if (!isProduction || disableBlacklist) { return logObject }
+
   const flatLogObj = _flattenObj(logObject)
+
   Object.keys(flatLogObj).forEach(key => {
     const value = flatLogObj[key]
     flatLogObj[key] = _blacklistKey(key, value)
   })
+
   logObject = _unflattenObj(flatLogObj)
   return logObject
 }
 
-function _flattenObj (obj, parent, res = {}) {
+function _flattenObj (obj, parent, flatObj = {}) {
   for (const key in obj) {
-    const propName = parent ? parent + '.' + key : key
+    const propName = parent ? `${parent}.${key}` : key
     if (typeof obj[key] === 'object') {
-      _flattenObj(obj[key], propName, res)
+      _flattenObj(obj[key], propName, flatObj)
     } else {
-      res[propName] = obj[key]
+      flatObj[propName] = obj[key]
     }
   }
-  return res
+  return flatObj
 }
 
 function _unflattenObj (data) {
-  const result = {}
-  for (const i in data) {
-    const keys = i.split('.')
-    keys.reduce(function (r, e, j) {
-      return r[e] || (r[e] = isNaN(Number(keys[j + 1])) ? (keys.length - 1 === j ? data[i] : {}) : [])
-    }, result)
+  const obj = {}
+
+  for (const flatKey in data) {
+    const flatKeys = flatKey.split('.')
+    flatKeys.reduce(function (acc, key, index) {
+      return acc[key] || (acc[key] = isNaN(Number(flatKeys[index + 1])) ? (flatKeys.length - 1 === index ? data[flatKey] : {}) : [])
+    }, obj)
   }
-  return result
+
+  return obj
 }
 
 function _blacklistKey (key, value) {
   const BLACKLIST_KEYS = global.API_LOGGER_BLACKLIST_KEYS || []
+
   for (const blacklistKey of BLACKLIST_KEYS) {
     const regex = new RegExp(`${blacklistKey}([.\\d]{2,})?$`, 'g')
-    const isBlacklistedKey = regex.test(key)
-    if (isBlacklistedKey) {
-      const encryptedValue = _encrypt(value)
-      return encryptedValue
+    if (regex.test(key)) {
+      return _encrypt(value)
     }
   }
 
@@ -62,6 +67,7 @@ function _encrypt (plainText = '') {
 
   const encryptor = crypto.createCipheriv('aes-128-cbc', keyBuffer, ivBuffer)
   const cipherTextBuffer = Buffer.concat([encryptor.update(`${plainText}`, 'utf8'), encryptor.final()])
+
   const cipherText = cipherTextBuffer.toString('base64')
   return cipherText
 }
