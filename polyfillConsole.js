3b62eb95ee0868a417381b61f8d73ab2a6b5b0be
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-03-07 13:46:57 +0530

    Fix Improts in polyfill

--- polyfillConsole.js
+++ polyfillConsole.js
@@ -1,13 +1,13 @@
-import logger from './logger.mjs'
+import apiLogger from './index.mjs'
 
-console.fatal = logger.fatal
-console.error = logger.error
-console.success = logger.success
-console.httpError = logger.httpError
-console.httpSuccess = logger.httpSuccess
-console.httpInfo = logger.httpInfo
-console.warn = logger.warn
-console.info = logger.info
-console.debug = logger.debug
-console.trace = logger.trace
-console.log = logger.log
+console.fatal = apiLogger.fatal
+console.error = apiLogger.error
+console.success = apiLogger.success
+console.httpError = apiLogger.httpError
+console.httpSuccess = apiLogger.httpSuccess
+console.httpInfo = apiLogger.httpInfo
+console.warn = apiLogger.warn
+console.info = apiLogger.info
+console.debug = apiLogger.debug
+console.trace = apiLogger.trace
+console.log = apiLogger.log
